02bb7c8a53eaf9fe4bd0fe62df77de42120a3858
aidanurp <14maret1998@gmail.com>
2017-11-08 18:06:11 +0700

    Menghitung Persegi Panjang dan Segitiga

--- latihan02.cpp
+++ latihan02.cpp
@@ -2,5 +2,30 @@
 using namespace std;
 
 int main(int argc, char** argv){
-
+		int pjg,lbr,l,pil,a,t;
+	
+	cout <<"1. Persegi Panjang\n";
+	cout <<"2. Segitiga\n";
+	cout <<"Masukkan Pilihan : ";
+	cin >>pil;
+	
+	switch(pil){
+		case 1:
+			cout<<"Masukkan Panjang = ";
+			cin >>pjg;
+			cout<<"Masukkan Lebar = ";
+			cin >>lbr;
+			l=pjg * lbr;
+			cout<<"Luas Persegi Panjang = "<<1<<"\n";
+			system ("Pause");
+			break;
+		case 2:
+			cout<<"Masukkan Alas Segitiga = ";
+			cin>>a;
+			cout<<"Masukkan Tinggi Segitiga = ";
+			cin>>t;
+			l=0.5*a*t;
+			cout<<"Luas Segitiga Adalah : "<<1<<"\n";
+			system ("Pause");
+	}
 }
