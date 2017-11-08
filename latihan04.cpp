1512bb1df5920d824be8857b9ffe4ae8b38fc0d7
aidanurp <14maret1998@gmail.com>
2017-11-08 18:25:09 +0700

    Menghitung Nilai Positif dan Negatif

--- latihan04.cpp
+++ latihan04.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main(){
+	int x;
+	cout << "Masukkan Nilai : ";
+	cin >>x;
 	
+	if (x>0){
+		cout <<"Nilai Positif";
+	}
+	else if (x<0){
+		cout <<"Nilai Negatif";
+	}
+	else{
+		cout <<"Nilai Nol";
+	}
 }
